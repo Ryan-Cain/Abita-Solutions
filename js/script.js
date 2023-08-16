178932eb5b1a807df260074647ef4af77b1fd4b9
rcain93 <84255629+rcain93@users.noreply.github.com>
2023-08-16 02:03:28 -0500

    added logo pics, worked on contact # movement, etc

--- js/script.js
+++ js/script.js
@@ -20,18 +20,37 @@ const phoneNum = document.getElementById("services-phone");
 
 window.onscroll = function () {
   checkScrollPosition();
+  console.log(window.scrollY);
 };
 function checkScrollPosition() {
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
   if (window.scrollY <= 500) {
     phoneNum.style.left = "55vw";
   } else if (window.scrollY >= 501 && window.scrollY <= 625) {
     phoneNum.style.left = "25vw";
     phoneNum.style.fontSize = "56px";
     phoneNum.style.marginTop = "0px";
-  } else if (window.scrollY >= 626) {
+  } else if (window.scrollY >= 625) {
     phoneNum.style.left = "25vw";
     phoneNum.style.fontSize = "36px";
     phoneNum.style.marginTop = "8px";
   }
 }
+
+// intersection observer
+
+const observerElement = document.querySelector("#observer");
+
+const options = {
+  root: document.body,
+  rootMargin: "0px",
+  threshold: 0,
+};
+
+function callback(entries, observer) {
+  console.log(observer);
+  console.log(entries[0].intersecting);
+}
+
+let observer = new IntersectionObserver(callback, options);
+observer.observe(observerElement);
